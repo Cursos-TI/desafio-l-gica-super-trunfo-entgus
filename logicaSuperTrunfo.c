afb6814bbb5621075d6154a84c58330c0195a762
entgus <gustavohen028@gmail.com>
2025-02-14 00:14:34 +0000

    comparaçao seletiva

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -11,7 +11,9 @@ int main() {
  int area;
  int pib; 
  int pontoturistico;  
-
+ int escolha;
+ 
+ 
 printf("cidade: \n", nome);
 scanf(" %s", &nome);
 
@@ -82,7 +84,78 @@ printf("ponto turistico: %d\n",  pontoturistico2);
 printf("densidade populacional:%.2f\n", densidadepopulacional2);
 printf("pib per capita:%.2f\n\n", pibpercapita2);
 
+printf("escolha o atributo a ser comparado\n");
+printf("1.populaçao\n");
+printf("2.area\n");
+printf("3.pib\n");
+printf("4.pontos turisticos\n");
+printf("5.densidade populacional\n");
+printf("6.pib per capita\n");
+printf("7.geral\n");
 
+printf("digite o numero:\n");
+
+scanf ("%d", &escolha);
+
+switch (escolha){
+case 1:
+if( populacao > populacao2){ 
+    printf("GANHADOR: %s POPULAÇAO: %d\n PERDEDOR: %s POPULAÇAO:%d\n", nome, populacao, nome2, populacao2);
+} else if (populacao2 > populacao) {
+
+    printf(" GANHADOR: %s POPULAÇAO: %d\n PERDEDOR: %s POPULAÇAO:%d \n", nome2, populacao2, nome,  populacao);
+}  else{
+    printf("empate");
+} 
+    break;
+case 2:
+if( area > area2){ 
+    printf("GANHADOR: %s area: %d\n PERDEDOR: %s area:%d\n", nome, area, nome2, area2);
+} else if (area2 > area) {
+
+    printf(" GANHADOR: %s area: %d\n PERDEDOR: %s area:%d \n", nome2, area2, nome,  area);
+}    else{
+    printf("empate");
+} 
+break;
+case 3:
+if( pib > pib2){ 
+    printf("GANHADOR: %s pib: %d\n PERDEDOR: %s pib:%d\n", nome, pib, nome2, pib2);
+} else if (pib2 > pib) {
+
+    printf(" GANHADOR: %s pib: %d\n PERDEDOR: %s pib:%d \n", nome2, pib2, nome,  pib);
+}  else{
+    printf("empate");
+}
+break;
+case 4: if( pontoturistico > pontoturistico2){ 
+    printf("GANHADOR: %s pontos turisticos: %d\n PERDEDOR: %s pontos turisticos:%d\n", nome, pontoturistico, nome2, pontoturistico2);
+} else if (pontoturistico2 > pontoturistico) {
+
+    printf(" GANHADOR: %s pontos turisticos: %d\n PERDEDOR: %s pontos turisticos:%d \n", nome2, pontoturistico2, nome,  pontoturistico);
+}else{
+        printf("empate");
+} 
+break;  
+case 5: if (!(densidadepopulacional > densidadepopulacional2)){ 
+    printf("GANHADOR: %s densidade populacional: %.2f\n PERDEDOR: %s densidade populacional:%f.2\n", nome, densidadepopulacional, nome2, densidadepopulacional2);
+} else if (!(densidadepopulacional2 > densidadepopulacional)) {
+
+    printf(" GANHADOR: %s densidade populacional: %.2f\n PERDEDOR: %s densidade populacional:%.2f \n", nome2, densidadepopulacional2, nome,  densidadepopulacional);
+}  else{
+    printf("empate");
+} 
+break; 
+case 6: if( pibpercapita > pibpercapita2){ 
+    printf("GANHADOR: %s pib per capita: %.2f\n PERDEDOR: %s pib per capita:%.2f\n", nome, pibpercapita, nome2, pibpercapita2);
+} else if (densidadepopulacional2 > densidadepopulacional) {
+
+    printf(" GANHADOR: %s pib per capita: %.2f\n PERDEDOR: %s pib per capita:%.2f \n", nome2, pibpercapita2, nome,  pibpercapita);
+}  else{
+    printf("empate");
+} 
+break; 
+case 7:
 int resultado1 = 0;
 int resultado2 = 0;
 
@@ -118,7 +191,7 @@ resultado1 += 1; ;
       resultado2 += 1;
 }
 
-if( densidadepopulacional > densidadepopulacional2){
+if( !(densidadepopulacional > densidadepopulacional2)){
 printf("%s densidade populacional +1\n", nome);
 resultado1 += 1;  
 }else{
@@ -144,8 +217,10 @@ printf("GANHADOR: %s +%d\n %s +%d\n", nome, resultado1, nome2, resultado2);
 } if (resultado1 == resultado2){
     printf(" EMPATE\n %s +%d\n %s +%d \n", nome, resultado1, nome2, resultado2);
 } 
+break;
 
-
-
-
+default:
+printf("invalido\n");
+    break;
+}
 }
